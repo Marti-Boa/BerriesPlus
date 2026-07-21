96d20f54abb6b61c3be30cf3fed2b1aff61e4dea
That1Slime9540 <jeffbessing@gmail.com>
2026-07-21 12:57:00 +0100

    Changed Hunger And Saturation Values

--- src/main/java/net/that1slime/berriesplus/item/ModFoodProperties.java
+++ src/main/java/net/that1slime/berriesplus/item/ModFoodProperties.java
@@ -5,7 +5,7 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoodProperties {
-    public static final FoodProperties BLUE_BERRY = new FoodProperties.Builder().nutrition(2).saturationModifier(0.20f).fast().build();
+    public static final FoodProperties BLUE_BERRY = new FoodProperties.Builder().nutrition(4).saturationModifier(0.20f).fast().build();
 
-    public static final FoodProperties YELLOW_BERRY = new FoodProperties.Builder().nutrition(5).saturationModifier(0.35f).fast().build();
+    public static final FoodProperties YELLOW_BERRY = new FoodProperties.Builder().nutrition(6).saturationModifier(0.7f).fast().build();
 }
